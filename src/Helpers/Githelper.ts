a5107ea1d1530f4ff98dd8860a51f1f027481dd5
nebosite <ericjorg@thejcrew.net>
2022-06-02 08:52:13 -0700

    Update githelper

--- src/Helpers/Githelper.ts
+++ src/Helpers/Githelper.ts
@@ -1,5 +1,84 @@
 import { RestHelper } from "./RestHelper";
 
+
+export interface GitCommit
+{
+    sha: string;
+    node_id: string;
+    commit: {
+       author: { name: string; email: string; date: string; },
+       committer: { name: string; email: string; date: string; },
+       message: string;
+       tree: { sha: string; url: string; },
+       url: string;
+       comment_count: number;
+       verification: { verified: boolean; reason: string; signature: string; payload: string; }
+    };
+    url: string;
+    html_url: string;
+    comments_url: string;
+    author: GitUser;
+    committer: GitUser;
+    parents: {sha: string; url: string; html_url: string;}[]
+}
+
+export interface GitReview
+{
+    id: number;
+    node_id: string;
+    user: GitUser;
+    body: string;
+    state: string;
+    html_url: string;
+    pull_request_url: string;
+    author_association: string;
+    _links: any;
+    //  {
+    //     "html": {
+    //         "href": "https://git.corp.adobe.com/WebPA/sparkler-shell/pull/53561#pullrequestreview-3230325"
+    //     },
+    //     "pull_request": {
+    //         "href": "https://git.corp.adobe.com/api/v3/repos/WebPA/sparkler-shell/pulls/53561"
+    //     }
+    // },
+    submitted_at: string;
+    commit_id: string;
+}
+
+export interface GitComment
+{
+    url: string;
+    pull_request_review_id: number;
+    id: number;
+    node_id: string;
+    diff_hunk: string;
+    path: string;
+    position: number;
+    original_position: number;
+    commit_id: string;
+    original_commit_id: string;
+    user: GitUser;
+    body: string;
+    created_at: string;
+    updated_at: string;
+    html_url: string;
+    issue_url: string;
+    pull_request_url: string;
+    author_association: string;
+    in_reply_to_id: number;
+    _links: any;
+    //  {
+    //     "self": {
+    //         "href": "https://git.corp.adobe.com/api/v3/repos/ejorgens/learning/pulls/comments/3218873"
+    //     },
+    //     "html": {
+    //         "href": "https://git.corp.adobe.com/ejorgens/learning/pull/11#discussion_r3218873"
+    //     },
+    //     "pull_request": {
+    //         "href": "https://git.corp.adobe.com/api/v3/repos/ejorgens/learning/pulls/11"
+    //     }
+    // }
+}
 export interface GitTag
 {
     name: string;
@@ -11,21 +90,144 @@ export interface GitTag
 
 export interface GitNodeData
 {
-  name: string;
-  path: string;
-  sha: string;
-  size: number;
-  url: string;
-  html_url: string;
-  git_url: string;
-  download_url: string | null;
-  type: string;
-  submodule_git_url: string;
-  _links: {
+    name: string;
+    path: string;
+    sha: string;
+    size: number;
+    url: string;
+    html_url: string;
+    git_url: string;
+    download_url: string | null;
+    type: string;
+    submodule_git_url: string;
+    _links: {
       self: string;
       git: string;
       html: string;
   }
+}
+
+export interface GitUser {
+    login: string;
+    id: number;
+    node_id: string;
+    avatar_url: string;
+    html_url: string;
+    type: string;
+    site_admin: boolean;
+}
+
+export interface GitRepo {
+    id: number;
+    node_id: string;
+    name: string;
+    full_name: string;
+    private: boolean;
+    owner: GitUser;
+    html_url: string;
+    description: string;
+    fork: boolean;
+    created_at: string;
+    updated_at: string;
+    pushed_at: string;
+    git_url: string;
+    ssh_url: string;
+    clone_url: string;
+    svn_url: string;
+    homepage: string;
+    size: number;
+    stargazers_count: number;
+    watchers_count: number;
+    language: string;
+    has_issues: boolean;
+    has_projects: boolean;
+    has_downloads: boolean;
+    has_wiki: boolean;
+    has_pages: boolean;
+    forks_count: number;
+    mirror_url: string;
+    archived: boolean;
+    disabled: boolean;
+    open_issues_count: number;
+    license: string;
+    forks: number;
+    open_issues: number;
+    watchers: number;
+    default_branch: string;
+}
+
+export interface GitPosition {
+    label: string;
+    ref: string;
+    sha: string;
+    user: GitUser;
+    repo: GitRepo;
+}
+
+export interface GitPullRequest {
+    id: number;
+    node_id: string;
+    html_url: string;
+    diff_url: string;
+    patch_url: string;
+    issue_url: string;
+    repository_url: string,
+    number: number;
+    state: string;
+    locked: boolean;
+    title: string;
+    user: GitUser;
+    
+    body: string;
+    created_at: string;
+    updated_at: string;
+    closed_at: string;
+    merged_at: string;
+    merge_commit_sha: string;
+    assignee: GitUser;
+    assignees: GitUser[];
+    requested_reviewers: GitUser[];
+
+    requested_teams: any[];
+    labels: any[];
+        // {
+        //     "id": 1294108,
+        //     "node_id": "MDU6TGFiZWwxMjk0MTA4",
+        //     "url": "https://git.corp.adobe.com/api/v3/repos/WebPA/sparkler-shell/labels/Team:Foundation-US",
+        //     "name": "Team:Foundation-US",
+        //     "color": "c2e0c6",
+        //     "default": false
+        // }
+    
+    milestone: string;
+    head: GitPosition;
+    base: GitPosition;
+    _links: any;
+    author_association: string;
+    merged: boolean;
+    mergeable: boolean;
+    rebaseable: boolean;
+    mergeable_state: string;
+    merged_by: GitUser;
+    comments: number;
+    review_comments: number;
+    maintainer_can_modify: boolean;
+    commits: number;
+    additions: number;
+    deletions: number;
+    changed_files: number; 
+
+    // Added later from another query
+    all_comments: GitComment[] | null;
+    all_commits: GitCommit[] | null;
+    reviews: GitReview[] | null;
+}
+
+interface GitSearchResponse
+{
+    total_count: number;
+    incomplete_results: boolean;
+    items: any[];
 }
 
 //------------------------------------------------------------------------------
@@ -35,14 +237,16 @@ export class GitFactory
 {
     server: string;
     apiToken: string;
+    apiPrefix: string;
 
     //------------------------------------------------------------------------------
     // ctor
     //------------------------------------------------------------------------------
-    constructor (server: string, apiToken: string)
+    constructor (server: string, apiPrefix: string, apiToken: string)
     {
         this.server = server;
         this.apiToken = apiToken;
+        this.apiPrefix = apiPrefix;
     }
 
     //------------------------------------------------------------------------------
@@ -50,7 +254,7 @@ export class GitFactory
     //------------------------------------------------------------------------------
     getRemoteRepo(repoOwner: string, repoName: string)
     {
-        return new GitRemoteRepo(this.server, repoOwner, repoName, this.apiToken);
+        return new GitRemoteRepo(this.server, this.apiPrefix, repoOwner, repoName, this.apiToken);
     }
 }
 
@@ -67,21 +271,38 @@ export class GitRemoteRepo
     _graphQLApi: RestHelper;
     _gitApiHelper: RestHelper;
 
+    get repoApi() {return `repos/${this.owner}/${this.name}`}
+    get contentsApi() {return `${this.repoApi}/contents`}
+    get tagsApi() {return `${this.repoApi}/tags`}
+    get searchApi() {return `search/issues`}
+
     //------------------------------------------------------------------------------
     // ctor
     //------------------------------------------------------------------------------
-    constructor (server: string, repoOwner: string, repoName: string, apiToken: string)
+    constructor (server: string, apiPrefix: string, repoOwner: string, repoName: string, apiToken: string)
     {
         this.cloneUrl = `git@${server}:${repoOwner}/${repoName}.git`
         this.name = repoName;
         this.owner = repoOwner;
         this.apiToken = apiToken;
 
-        this._graphQLApi = new RestHelper(`https://${server}/api/graphql`)
+        this._graphQLApi = new RestHelper(`https://${server}/api/graphql`, undefined, apiPrefix)
         this._graphQLApi.addHeader("Authorization", "token " + this.apiToken);
 
-        this._gitApiHelper = new RestHelper(`https://${server}/api/v3/repos/${repoOwner}/${repoName}`);
+        const endPoint = server.toLowerCase() === "github.com"  
+            ? `https://api.github.com/`
+            : `https://${server}/api/v3/`
+
+        this._gitApiHelper = new RestHelper(endPoint, undefined, apiPrefix);
         this._gitApiHelper.addHeader("Authorization", "token " + this.apiToken);
+    }
+
+    //------------------------------------------------------------------------------
+    // Get tags on this reqpo
+    //------------------------------------------------------------------------------
+    async testConnection() {
+        const result = await this._gitApiHelper.restGet<any>(`repos/${this.owner}/${this.name}`)
+        if(result.name !== this.name) throw new Error(`Unexpected repo name: ${result.name}`)
     }
 
     //------------------------------------------------------------------------------
@@ -91,7 +312,7 @@ export class GitRemoteRepo
     {
         for(let page = 0; page < 1000; page++)
         {
-            const result = await this._gitApiHelper.restGet<GitTag[]>("/tags?per_page=100&page=" + page);
+            const result = await this._gitApiHelper.restGet<GitTag[]>(`${this.tagsApi}?per_page=100&page=` + page);
             if(!result) break;
             for(const tag of result)
             {
@@ -105,15 +326,52 @@ export class GitRemoteRepo
     //------------------------------------------------------------------------------
     // Return git content details for a file
     //------------------------------------------------------------------------------
-    async getFile(branchOrSha: string, path: string): Promise<GitNodeData>
+    async getFile(branchOrSha: string, path: string): Promise<GitNodeData | null>
     {
         if(!path.startsWith("/")) path = "/" + path;
 
-        return await this._gitApiHelper.restGet<GitNodeData>(`/contents${path}?ref=${branchOrSha}&per_page=1`);
+        return await this._gitApiHelper.restGet<GitNodeData>(`${this.contentsApi}${path}?ref=${branchOrSha}&per_page=1`);
     }
 
     //------------------------------------------------------------------------------
-    // Return git content details for a directory
+    // Return git content details for a file
+    //------------------------------------------------------------------------------
+    async getPullRequest(prNumber: number | string): Promise<GitPullRequest | null>
+    {
+        const returnMe = await this._gitApiHelper.restGet<GitPullRequest>(`${this.repoApi}/pulls/${prNumber}`);
+        if(!returnMe) return null;
+        const codeComments = await this._gitApiHelper.restGet<GitComment[]>(`${this.repoApi}/pulls/${prNumber}/comments`)
+        const prComments = await this._gitApiHelper.restGet<GitComment[]>(`${this.repoApi}/issues/${prNumber}/comments`)
+        returnMe.all_commits = await this._gitApiHelper.restGet<GitCommit[]>(`${this.repoApi}/pulls/${prNumber}/commits`)
+        returnMe.reviews = await this._gitApiHelper.restGet<GitReview[]>(`${this.repoApi}/pulls/${prNumber}/reviews`);
+        if(returnMe.reviews)
+        {
+            const meaningfulReviews = returnMe.reviews.filter(r => r.body !== "" || r.state !== "COMMENTED");
+            returnMe.reviews = meaningfulReviews ? meaningfulReviews : null;
+        }
+        let allComments = new Array<GitComment>();
+        if(codeComments) allComments = allComments.concat(codeComments);
+        if(prComments) allComments = allComments.concat(prComments);
+        returnMe.all_comments = allComments;
+        return returnMe;
+    }
+
+    //------------------------------------------------------------------------------
+    // Return all the pull requests
+    // Filter options:
+    //      involvesUser:  return all prs that involve the specified user
+    //------------------------------------------------------------------------------
+    async getPullRequests(filter: {involvesUser?: string}): Promise<GitPullRequest[] | null>
+    {
+        let query = "?per_page=50&q=type:pr+sort:updated-desc";
+        if(filter.involvesUser) query += `+involves:${filter.involvesUser}`;
+        const response = await this._gitApiHelper.restGet<GitSearchResponse>(`${this.searchApi}${query}`);
+        if(!response) return null;
+        return response.items as GitPullRequest[];
+    }
+
+    //------------------------------------------------------------------------------
+    // Return the children of a specifice node path
     //------------------------------------------------------------------------------
     async getNodeChildren(branchOrSha: string, path: string): Promise<GitNodeData[]>
     {
@@ -122,7 +380,7 @@ export class GitRemoteRepo
 
         for(let page = 0; page < 1000; page++)
         {
-            const result = await this._gitApiHelper.restGet<GitNodeData[]>(`/contents${path}?ref=${branchOrSha}&per_page=100&page=` + page);
+            const result = await this._gitApiHelper.restGet<GitNodeData[]>(`${this.contentsApi}${path}?ref=${branchOrSha}&per_page=100&page=` + page);
             if(!result) break;
             for(const item of result)
             {
@@ -151,6 +409,7 @@ export class GitRemoteRepo
         //console.log(query);
         const result = await this._graphQLApi.restPost<{data: {repository: {object: {text: string}}}}>("", JSON.stringify({query}))
         //console.log(JSON.stringify(result));
+        if(!result) return null;
         return result.data.repository.object.text;
     }
 
